67c24c608be6315816edcf6a45e27b7432efc942
surya1701 <suryavardan17@gmail.com>
2021-03-02 12:52:04 +0530

    Added django folders with nav and google api

---
+++ donor/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('about', views.about, name="about"),
+    path('logout', views.logout, name="logout"),
+    path('accounts/', include('allauth.urls')),
+]
+
+# aa
