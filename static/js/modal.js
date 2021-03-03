761b3eefa7fa0234adf5c654169706e0257c9f73
surya1701 <suryavardan17@gmail.com>
2021-03-03 09:41:36 +0530

    Logout modal

--- static/js/modal.js
+++ static/js/modal.js
@@ -1,25 +1,32 @@
-// Get the modal
 var modal = document.getElementById("myModal");
+var l_modal = document.getElementById("logoutModal");
 
-// Get the button that opens the modal
+
 var btn = document.getElementById("myBtn");
+var l_btn = document.getElementById("logout-link");
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+var l_span = document.getElementsByClassName("close")[1];
 
-// When the user clicks the button, open the modal
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
+  l_modal.style.display = "none";
 }
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+l_btn.onclick = function () {
+  modal.style.display = "none";
+  l_modal.style.display = "block";
+}
+span.onclick = function () {
   modal.style.display = "none";
 }
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+l_span.onclick = function () {
+  l_modal.style.display = "none";
+}
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+  if (event.target == l_modal) {
+    l_modal.style.display = "none";
   }
 }
