318acc9b2540422aa90285df8c97286e50309dc0
surya1701 <suryavardan17@gmail.com>
2021-03-24 10:09:56 +0530

    Donor database

--- donor/models.py
+++ donor/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Users(models.Model):
+    # user_id = models.UUIDField(
+    #     primary_key=True, default=uuid.uuid4, editable=False)
+    # autofield
+    email = models.EmailField()
+    total_amt = models.PositiveIntegerField(default=0)
--- donor/views.py
+++ donor/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
+from donor.models import Users
 
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    if request.user.is_authenticated:
+        if not Users.objects.filter(email=request.user.email).exists():
+            u = Users(email=request.user.email)
+            u.save()
+        u = Users.objects.get(email=request.user.email)
+        u_dict = {'email': u.email, 'amt': u.total_amt}
+        print(u.pk)
+        return render(request, "index.html", u_dict)
+    else:
+        return render(request, "index.html")
 
 
 def about(request):
     return render(request, "about.html")
-
 
 
 def logout(request):
