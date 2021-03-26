f230a7ce40c1c3758b07deedaf99e854ed4b1bb3
surya1701 <suryavardan17@gmail.com>
2021-03-26 14:51:27 +0530

    User profile

--- donor/views.py
+++ donor/views.py
@@ -3,21 +3,31 @@ from donor.models import Users
 
 
 # Create your views here.
-def index(request):
+def get_user(request):
     if request.user.is_authenticated:
         if not Users.objects.filter(email=request.user.email).exists():
             u = Users(email=request.user.email)
             u.save()
         u = Users.objects.get(email=request.user.email)
-        u_dict = {'email': u.email, 'amt': u.total_amt}
         print(u.pk)
+        u_dict = {'email': u.email, 'amt': u.total_amt}
+    return u_dict
+
+
+def index(request):
+    if request.user.is_authenticated:
+        u_dict = get_user(request)
         return render(request, "index.html", u_dict)
     else:
         return render(request, "index.html")
 
 
 def about(request):
-    return render(request, "about.html")
+    if request.user.is_authenticated:
+        u_dict = get_user(request)
+        return render(request, "about.html", u_dict)
+    else:
+        return render(request, "about.html")
 
 
 def logout(request):
