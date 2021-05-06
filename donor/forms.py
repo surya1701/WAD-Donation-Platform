68f0ccb79f98c3c1f50ab9daec806998a1f5456f
surya1701 <suryavardan17@gmail.com>
2021-05-06 22:29:52 +0530

    Causes edits

--- donor/forms.py
+++ donor/forms.py
@@ -5,8 +5,9 @@ from django import forms
 
 class ContactForm(forms.Form):
     first_name = forms.CharField(max_length=50, required=True, widget=forms.TextInput(
-        attrs={'class': 'form-control', 'placeholder': 'Name'}))
-    # mobile = forms.NumberInput(required=True)
+        attrs={'class': 'form-control', 'placeholder': 'First Name'}))
+    second_name = forms.CharField(max_length=50, required=True, widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Second Name'}))
     email_address = forms.EmailField(max_length=150, required=True, widget=forms.EmailInput(
         attrs={'class': 'form-control', 'placeholder': 'Email'}))
     message = forms.CharField(widget=forms.Textarea(
