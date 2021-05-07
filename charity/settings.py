9a8af648fd42e62d201d7a6e1a95eb715be3a8b1
surya1701 <suryavardan17@gmail.com>
2021-05-07 21:32:20 +0530

    Update settings.py

--- charity/settings.py
+++ charity/settings.py
@@ -145,7 +145,7 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
 )
-SITE_ID = 1
+SITE_ID = 2
 LOGIN_REDIRECT_URL = '/'
 ACCOUNT_LOGOUT_REDIRECT_URL = '/'
 # ******* might have to change cause unsafe
